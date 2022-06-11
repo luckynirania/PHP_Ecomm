cb31f0ca25c6902d1807607ab6313d2418669b51
Lokesh Nirania <lknmahan@gmail.com>
2022-06-11 11:49:23 +0530

    .

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PHP_Ecomm
+
+create a table in phpmyadmin with name ```ecomm``` and import  ```database/ecomm.sql```
+
+Username: admin@admin.com
+Password: password
+
+USER LOGIN:
+
+Username: aman@luthra.com
+Password: code0
--- includes/navbar.php
+++ includes/navbar.php
@@ -12,8 +12,6 @@
       <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
         <ul class="nav navbar-nav">
           <li><a href="index.php">HOME</a></li>
-          <li><a href="about_us.php">ABOUT US</a></li>
-          <li><a href="contact_us.php">CONTACT US</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORY <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
@@ -38,6 +36,8 @@
               ?>
             </ul>
           </li>
+          <li><a href="about_us.php">ABOUT US</a></li>
+          <li><a href="contact_us.php">CONTACT US</a></li>
         </ul>
         <form method="POST" class="navbar-form navbar-left" action="search.php">
           <div class="input-group">
